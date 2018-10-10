1b4e0f07e89a6a0e65ce41539ac3e7550d0f03f5
santiago rosero <santiago rosero@172.30.184.182>
2018-10-10 18:15:07 -0500

    edit the document and create the palyer class

---
+++ BALONCESTO/Modelo/Player.java
@@ -0,0 +1,30 @@
+package Modelo;
+
+public class Player {
+	
+	private String name;
+	private int years;
+	private String team;
+	private int matchs;
+	private double matchPoints;
+	private int matchRebounds;
+	private int matchAssistances;
+	private int matchTheft;
+	private int matchBlocking;
+	
+	public Player(String n, int y, String t, int m, double p, int r, int a, int h, int b){
+		name=n;
+		years=y;
+		team=t;
+		matchs=m;
+		matchpoints=p;
+		matchRebounds=r;
+		matchAssistances=a;
+		matchTheft=h;
+		matchBlocking=b;
+	}
+	
+	
+	
+
+}
