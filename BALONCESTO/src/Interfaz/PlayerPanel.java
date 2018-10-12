eb9cfb02cb8ea9a2c1961c6a67e4990948a44231
Daniela Otero <1193476661@SC203E20.academico.icesi.edu.co>
2018-10-12 12:57:05 -0500

    process saved.

--- BALONCESTO/src/Interfaz/PlayerPanel.java
+++ BALONCESTO/src/Interfaz/PlayerPanel.java
@@ -7,16 +7,32 @@ import javax.swing.*;
 public class PlayerPanel extends JPanel {
 	
 	
-	private JScrollBar bar;
+
 	private PrincipalWindow pw;
+	private List l;
+	private JList list;
 	
 	public PlayerPanel(PrincipalWindow pw) {
 		
 		this.pw = pw;
+		l = new List();
+
+		for(int i=1; i<=100; i++){
+			
+			l.add("Jugador" + i);
+		}
 		
-		bar = new JScrollBar();
-		setSize(100,200);
-		add(bar);
+		
+		
+	
+		JPanel aux = new JPanel();
+		
+		list = new JList();
+		list.add(l);
+		
+		setLayout(new BorderLayout());
+		add(l, BorderLayout.CENTER);
+		
 		
 	}
 	
--- BALONCESTO/src/Interfaz/PrincipalWindow.java
+++ BALONCESTO/src/Interfaz/PrincipalWindow.java
@@ -6,25 +6,27 @@ import javax.swing.*;
 public class PrincipalWindow extends JFrame{
 
 	private PlayerPanel pPlayer;
-	private JList list;
+
 	private String data;
 
 	public PrincipalWindow() {
 		
 		pPlayer = new PlayerPanel(this);
 		data = "Hola";
-		list = new JList();
+
 		
 		
 		setSize(1000,700);
 		setResizable(false);
 		setTitle("B A L O N C E S T O ~ ~ ~ ~ ~ ~ ~ ~  B I E N V E N I D O  ~ ~ ~ ~ ~ ~ ~ ~  B A L O N C E S T O");
 		
-		add(list);
-		add(pPlayer);
+		setLayout(new BorderLayout());
+		add(pPlayer,BorderLayout.CENTER);
 		
 		
 	}
+	
+	
 	
 	
 	
